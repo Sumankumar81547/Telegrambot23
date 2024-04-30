260040ff52c20d1f15296c8f8c244e34ed2d1231
Sumankumar81547 <168259000+Sumankumar81547@users.noreply.github.com>
2024-04-30 07:50:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,18 +14,18 @@ API_HASH = getenv("API_HASH", None)
 STRING_SESSION = getenv("STRING_SESSION", None)
 
 ### ❖ ➥
-OWNER_ID = int(getenv("OWNER_ID", "6922271843"))
+OWNER_ID = int(getenv("OWNER_ID", "6615076069"))
 
 ### ❖ ➥
 MONGO_URL = getenv("MONGO_URL", None)
 
 ### ❖ ➥
-SUPPORT_GRP = getenv("SUPPORT_GRP", "THE_FRIENDZ")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "R_I_S_I_NG")
 
 ### ❖ ➥
-UPDATE_CHNL = getenv("UPDATE_CHNL", "ROY_EDITX")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "Log_support_bott")
 
 ### ❖ ➥
-OWNER_USERNAME = getenv("OWNER_USERNAME", "LOCO_X_PILOT")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "@Mohitkumar076")
 
 
